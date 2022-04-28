8caebec13b9d8240884ce8b145f66391e4249e62
Reganmatics <devreganmatics@gmail.com>
2022-04-28 22:28:51 +0100

    anything wey happen

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,48 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int.
- * @b: pointer to a string containing a binary number
+ * binary_to_uint - converts binary number to an unsigned int
+ * @b: the string containing 0's and 1's to be converted to integer
  *
- * Return: unsigned int with decimal value of binsry number, or 0 if error
+ * Return: integer converted from binary
+ *	   0 if @b contains a character other than 0 and 1
+ *	   @b is NULL
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int num;
+	unsigned int i, uint = 0, digits = 0;
 
-	num = 0;
 	if (!b)
 		return (0);
-	for (i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i] != 0; i++)
+		digits++;
+	digits--;
+
+	for (i = 0; b[i] != 0; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] != 48 && b[i] != 49)
 			return (0);
+		uint += (b[i] - 48) *  power(2, digits - i);
 	}
-	for (i = 0; b[i] != '\0'; i++)
+
+	return (uint);
+}
+
+/**
+ * power - raise num by the exponent power
+ * @num: number to be raised
+ * @power: the exponent
+ *
+ * Return: the exponentiated number
+ */
+int power(int num, int power)
+{
+	int result = 1;
+
+	for (; power > 0; power--)
 	{
-		num <<= 1;
-		if (b[i] == '1')
-			num += 1;
+		result = result * num;
 	}
-	return (num);
+	return (result);
 }
