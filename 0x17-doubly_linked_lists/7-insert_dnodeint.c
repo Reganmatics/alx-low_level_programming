3d8184b500b06e7ec2101fa35716d6525d01c87e
Reganmatics <devreganmatics@gmail.com>
2022-06-10 21:42:30 +0100

    fix checker issues

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,56 +1,54 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node of type dlistint_t at the
- * given position @idx
- * @h: the first element of the doubly linked list
- * @idx: the position of the newly added element
- * @n: the data of the new element
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
  *
- * Return: address of the new node, or NULL if it fails or is not possible to
- * add the new node at @idx
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	dlistint_t *h_copy = *h;
-	unsigned int node = 0;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-	if (!*h)
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
 	{
-		*h = new_node;
-		return (new_node);
-	}
-	while (h_copy)
-	{
-		if (idx == node)
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
 		{
-			new_node->n = n;
-			new_node->prev = NULL;
-			new_node->next = NULL;
-			if (!h_copy->prev)
+			if (i == idx)
 			{
-				h_copy->prev = new_node;
-				new_node->next = h_copy;
-				*h = new_node;
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
 			}
-			else if (!h_copy->next)
-			{
-				h_copy->next = new_node;
-				new_node->prev = h_copy;
-			}
-			else
-			{
-				new_node->next = h_copy;
-				new_node->prev = h_copy->prev;
-				h_copy->prev->next = new_node;
-				h_copy->prev = new_node;
-			}
-			return (new_node);
+			head = head->next;
+			i++;
 		}
-		node++;
-		h_copy = h_copy->next;
 	}
-	return (NULL);
+
+	return (new);
 }
