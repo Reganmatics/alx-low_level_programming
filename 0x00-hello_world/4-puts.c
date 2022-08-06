b0002fa6b5ef88b61665fc5bce6e6bee132996d5
Reganmatics <devreganmatics@gmail.com>
2022-08-06 22:47:08 +0100

    add task 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function entry point
+ * Description: this function displays the dtring within the string quotes
+ * Return: always (0) as an indicator of success
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
