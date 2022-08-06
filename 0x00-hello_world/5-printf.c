92d45ec084cd329047801159a7e1ebcde62b8459
Reganmatics <devreganmatics@gmail.com>
2022-08-06 22:58:31 +0100

    add task 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- function entry point
+ * Description: displays the text within string quotes to the console
+ * Return: always (0) as an indicator of success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
